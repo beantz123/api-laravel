2f1e241a4b6ce91c4dde934bed6f3f28fb54bb21
beantz123 <trizmours2002@gmail.com>
2024-09-16 22:29:57 -0300

    segundo comm

--- produtos_app/routes/api.php
+++ produtos_app/routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProdutosController::class, "index"]);
 Route::post('/products', [ProdutosController::class, "store"]);
-Route::put('/products/{productId}', [ProdutosController::class, "update"]);
-Route::get('/products/{productId}', [ProdutosController::class, "viewProduct"]);
-Route::delete('/products/{productId}', [ProdutosController::class, "delete"]);
+Route::put('/products/{productId}', [ProdutosController::class, "update"])->where('productId', '[0-9]+');
+Route::get('/products/{productId}', [ProdutosController::class, "viewProduct"])->where('productId', '[0-9]+');
+Route::delete('/products/{productId}', [ProdutosController::class, "delete"])->where('productId', '[0-9]+');
